bfeb1e4da2198cfba485010378f0cc11c0c0e182
dlxx <dlxxdlcc@gmail.com>
2022-07-17 17:24:52 +0800

    svg loader

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,15 +1,39 @@
 <template>
-    <div>
-      <router-link to="/money">记账</router-link>
-      <router-link to="/labels">标签</router-link>
-      <router-link to="/statistics">统计</router-link>
-    </div>
+  <div>
+    <router-link to="/money">
+      <svg>
+        <use xlink:href="#money"/>
+      </svg>
+      记账
+    </router-link>
+    <router-link to="/labels">
+      <svg>
+        <use xlink:href="#label"/>
+      </svg>
+      标签
+    </router-link>
+    <router-link to="/statistics">
+      <svg>
+        <use xlink:href="#statistics"/>
+      </svg>
+      统计
+    </router-link>
+
+  </div>
 </template>
 
 <script lang='ts'>
+import a from '@/assets/icons/label.svg';
+import b from '@/assets/icons/money.svg';
+import c from '@/assets/icons/statistics.svg';
+
+console.log(a);
+console.log(b);
+console.log(c);
+
 export default {
-name: "Nav"
-}
+  name: 'Nav'
+};
 </script>
 
 <style lang='scss' scoped>
