680e72dc6a8a63a1453cae8ec142e70a24ca9580
dlxx <dlxxdlcc@gmail.com>
2022-07-17 23:16:00 +0800

    nav css

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,13 @@
 
 <style lang="scss">
 @import "~@/assets/normalize.css";
+body{
+  line-height: 1.5;
+}
+a{
+  text-decoration: none;
+  color: inherit;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -31,7 +38,6 @@
   padding: 30px;
 
   a {
-    font-weight: bold;
     color: #2c3e50;
 
     &.router-link-exact-active {
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,41 +1,48 @@
 <template>
-  <div>
-    <router-link to="/money">
-      <svg>
-        <use xlink:href="#money"/>
-      </svg>
+  <nav>
+    <router-link to="/money" class="item">
+      <Icon name="money"></Icon>
       记账
     </router-link>
-    <router-link to="/labels">
-      <svg>
-        <use xlink:href="#label"/>
-      </svg>
+    <router-link to="/labels" class="item">
+      <Icon name="labels"></Icon>
       标签
     </router-link>
-    <router-link to="/statistics">
-      <svg>
-        <use xlink:href="#statistics"/>
-      </svg>
+    <router-link to="/statistics" class="item">
+      <Icon name="statistics"></Icon>
       统计
     </router-link>
 
-  </div>
+  </nav>
 </template>
 
 <script lang='ts'>
-import a from '@/assets/icons/label.svg';
-import b from '@/assets/icons/money.svg';
-import c from '@/assets/icons/statistics.svg';
-
-console.log(a);
-console.log(b);
-console.log(c);
-
 export default {
   name: 'Nav'
 };
 </script>
 
 <style lang='scss' scoped>
+nav {
+  display: flex;
+  box-shadow: 0 0 3px rgb(0, 0, 0, 0.25);
+  font-size: 16px;
+
+
+  > .item {
+    width: 33.3333%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+
+    .icon {
+      padding: 2px 0;
+      width: 32px;
+      height: 32px;
+    }
+
+  }
+}
 
 </style>
